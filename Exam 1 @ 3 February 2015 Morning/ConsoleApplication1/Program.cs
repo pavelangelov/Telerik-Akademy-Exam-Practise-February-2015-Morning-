d3a1950330d2691c59ac8b9fe9520f17ba4b4555
pavelangelov <p_a_v_el@abv.bg>
2016-04-15 12:55:07 +0300

    Solved task 3

---
+++ Exam 1 @ 3 February 2015 Morning/ConsoleApplication1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            List<string> inputNumbers = new List<string>();
+            while (input != "END")
+            {
+                inputNumbers.Add(input);
+                input = Console.ReadLine();
+            }
+            if (inputNumbers.Count <= 10)
+            {
+                BigInteger result = 1;
+                for (int i = 1; i < inputNumbers.Count; i += 2)
+                {
+                    string num = inputNumbers[i];
+                    ulong product = 1U;
+                    for (int j = 0; j < num.Length; j++)
+                    {
+                        ulong currentNum = (ulong)(num[j] - '0');
+                        if (currentNum != 0)
+                        {
+                            product *= currentNum;
+                        }
+                    }
+                    result *= product;
+                }
+
+                Console.WriteLine(result);
+            }
+            else
+            {
+                BigInteger productOfFirstTen = 1;
+                BigInteger productOfTheRest = 1;
+                for (int i = 1; i < inputNumbers.Count; i += 2)
+                {
+                    string num = inputNumbers[i];
+                    ulong product = 1U;
+                    for (int j = 0; j < num.Length; j++)
+                    {
+                        ulong currentNum = (ulong)(num[j] - '0');
+                        if (currentNum != 0)
+                        {
+                            product *= currentNum;
+                        }
+                    }
+                    if (i < 10)
+                    {
+                        productOfFirstTen *= product;
+                    }
+                    else
+                    {
+                        productOfTheRest *= product;
+                    }
+                }
+                Console.WriteLine(productOfFirstTen);
+                Console.WriteLine(productOfTheRest);
+            }
+
+        }
+    }
+}
